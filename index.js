5d9966d4d1b2debef5ac9e4eec2105b752323c9e
Nidhi Baldia <117630401+DevOps3124@users.noreply.github.com>
2022-11-18 14:21:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const aws = require("aws-sdk");
 const ses = new aws.SES({ region: "us-east-1" });
 exports.handler = async function (event) {
   console.log('EVENT: ', event)
+	// Extract the properties from the event body
+  const { senderEmail, senderName, message } = JSON.parse(event.body)
   const params = {
     Destination: {
-      ToAddresses: ["your@email.com"],
+      ToAddresses: ["nidhi.b@webknot.in"],
     },
+		// Interpolate the data in the strings to send
     Message: {
       Body: {
         Text: { 
-            Data: `Hello from Lambda!` 
+            Data: `You just got a message from ${senderName} - ${senderEmail}:
+            ${message}` 
         },
       },
-      Subject: { Data: `Message from AWS Lambda` },
+      Subject: { Data: `Message from ${senderName}` },
     },
-    Source: "nidhibaldia@email.com",
+    Source: "nidhibaldia@gmail.com",
   };
 
-  return ses.sendEmail(params).promise()
+  return ses.sendEmail(params).promise();
 };
